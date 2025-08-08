573c284b8dd488fc95b38b771a041287b080c16e
Valverde <96694746+Valverde-101@users.noreply.github.com>
2025-08-08 15:31:02 -0500

    Revert "Fix icon path resolution for Expo app"

--- MiAppNevera/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-
-const config = getDefaultConfig(__dirname);
-
-// Include the icons folder located outside of the project root
-config.watchFolders = [path.resolve(__dirname, '..', 'icons')];
-
-module.exports = config;
