1eae9217eb7e5ed30a79a85ee67030f63ec7a975
Valverde <96694746+Valverde-101@users.noreply.github.com>
2025-08-10 14:43:30 -0500

    Add recipe book feature

--- MiAppNevera/App.js
+++ MiAppNevera/App.js
@@ -5,6 +5,9 @@ import InventoryScreen from './src/screens/InventoryScreen';
 import ShoppingListScreen from './src/screens/ShoppingListScreen';
 import { InventoryProvider } from './src/context/InventoryContext';
 import { ShoppingProvider } from './src/context/ShoppingContext';
+import RecipeBookScreen from './src/screens/RecipeBookScreen';
+import RecipeDetailScreen from './src/screens/RecipeDetailScreen';
+import { RecipeProvider } from './src/context/RecipeContext';
 import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
@@ -13,21 +16,33 @@ export default function App() {
   return (
     <InventoryProvider>
       <ShoppingProvider>
-        <NavigationContainer>
-          <StatusBar style="auto" />
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Inventory"
-              component={InventoryScreen}
-              options={{ title: 'Nevera' }}
-            />
-            <Stack.Screen
-              name="Shopping"
-              component={ShoppingListScreen}
-              options={{ title: 'Compras' }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <RecipeProvider>
+          <NavigationContainer>
+            <StatusBar style="auto" />
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Inventory"
+                component={InventoryScreen}
+                options={{ title: 'Nevera' }}
+              />
+              <Stack.Screen
+                name="Shopping"
+                component={ShoppingListScreen}
+                options={{ title: 'Compras' }}
+              />
+              <Stack.Screen
+                name="Recipes"
+                component={RecipeBookScreen}
+                options={{ title: 'Recetario' }}
+              />
+              <Stack.Screen
+                name="RecipeDetail"
+                component={RecipeDetailScreen}
+                options={{ title: 'Receta' }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </RecipeProvider>
       </ShoppingProvider>
     </InventoryProvider>
   );
