70797fa61c3aa697b20923b8a37f0b844c54a151
Valverde <96694746+Valverde-101@users.noreply.github.com>
2025-08-18 21:47:57 -0500

    Revert "Configure Google authentication for web and Android"

--- MiAppNevera/src/screens/UserDataScreen.js
+++ MiAppNevera/src/screens/UserDataScreen.js
@@ -16,7 +16,6 @@ import { useCustomFoods } from '../context/CustomFoodsContext';
 import { exportBackup, importBackup } from '../utils/backup';
 import { useTheme, useThemeController } from '../context/ThemeContext';
 import * as WebBrowser from 'expo-web-browser';
-import * as AuthSession from 'expo-auth-session';
 import * as Google from 'expo-auth-session/providers/google';
 import { uploadBackupToGoogleDrive, downloadBackupFromGoogleDrive } from '../utils/googleDrive';
 import * as Updates from 'expo-updates';
@@ -52,11 +51,9 @@ export default function UserDataScreen() {
   const [uploading, setUploading] = useState(false);
   const [downloading, setDownloading] = useState(false);
   const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: '388689708365-54q3jlb6efa8dm3fkfcrbsk25pb41s27.apps.googleusercontent.com',
-    androidClientId: '388689708365-4g4lnv5ilksj12cghfa17flc68c5d5qk.apps.googleusercontent.com',
-    webClientId: '388689708365-54q3jlb6efa8dm3fkfcrbsk25pb41s27.apps.googleusercontent.com',
+    clientId: '388689708365-54q3jlb6efa8dm3fkfcrbsk25pb41s27.apps.googleusercontent.com',
     scopes: ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'],
-    redirectUri: AuthSession.makeRedirectUri({ scheme: 'miappnevera' }),
+    redirectUri: Platform.select({ web: window.location.origin, default: undefined }),
   });
 
   useEffect(() => {
