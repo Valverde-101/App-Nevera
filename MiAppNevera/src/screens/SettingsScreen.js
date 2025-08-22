2f2ba57b8de217f85314fb9489c9f05f932e8294
Valverde <96694746+Valverde-101@users.noreply.github.com>
2025-08-22 18:45:08 -0500

    feat: add membership screen

--- MiAppNevera/src/screens/SettingsScreen.js
+++ MiAppNevera/src/screens/SettingsScreen.js
@@ -46,6 +46,11 @@ export default function SettingsScreen({ navigation }) {
           <Text style={styles.itemTitle}>Datos de usuario</Text>
           <Text style={styles.itemDesc}>Respaldos, importación y eliminación total.</Text>
         </TouchableOpacity>
+
+        <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Memberships')}>
+          <Text style={styles.itemTitle}>Membresías</Text>
+          <Text style={styles.itemDesc}>Accede a funciones extra mediante suscripción.</Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
